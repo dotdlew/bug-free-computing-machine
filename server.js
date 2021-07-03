9e419e369da30738ac7f4849ee8fc0141759a0ad
Daniel Lewis <daniel.lewis@protonmail.com>
2021-07-03 12:19:59 -0600

    mvp

---
+++ server.js
@@ -0,0 +1,80 @@
+const PORT = process.env.PORT || 3000;
+const { notes } = require("./db/db.json");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+
+// filter queries
+function filteredQuery(query, notesArr) {
+  let filtered = notesArr;
+
+  if (query.title) {
+    filtered = filtered.filter((notes) => notes.title === query.title);
+  }
+
+  if (query.text) {
+    filtered = filtered.filter((notes) => notes.text === query.text);
+  }
+
+  return filtered;
+}
+
+// create notes
+function createNote(body, notesArr) {
+  let note = body;
+  notesArr.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify({ notes: notesArr }, null, 2)
+  );
+  return note;
+}
+
+// delete notes
+function deleteNote(id, notesArr) {}
+
+// GET HOME API route
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// GET NOTES API
+app.get("/api/notes", function (req, res) {
+  let results = notes;
+  if (req.query) {
+    results = filteredQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+// POST NOTES API
+app.post("/api/notes", function (req, res) {
+  req.body.id = notes.length.toString();
+  const note = createNote(req.body, notes);
+  res.json(req.body);
+});
+
+// DELETE NOTES API
+app.delete("/api/notes/:id", function (req, res) {
+  let results = notes;
+});
+
+// GET notes route
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// GET catchall route
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.listen(PORT, function () {
+  console.log(`listening on port ${PORT}`);
+});
